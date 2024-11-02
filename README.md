8b2c5224d92ab1693243249b20608c4f17d29cdf
Fernando <asialer05@hotmail.com>
2024-11-02 18:28:34 -0500

    Proyecto de base de users-service

---
+++ README.md
@@ -0,0 +1,2 @@
+# Servicio de usuarios de DFOOD
+> Este servicio se encargara de gestionar los usuarios de CLIENTES Y REPARTIDORES. 
---
+++ src/test/java/com/fernando/ms/users/app/dfood_users_service/DfoodUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.users.app.dfood_users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfoodUsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
