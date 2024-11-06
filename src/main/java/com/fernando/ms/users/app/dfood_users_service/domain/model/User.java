bdea1d7d0d725e68d2c8bf17e3373f6881c7a840
Fernando <asialer05@hotmail.com>
2024-11-06 11:36:36 -0500

    Se agrego el atributo fullName en User

--- src/main/java/com/fernando/ms/users/app/dfood_users_service/domain/model/User.java
+++ src/main/java/com/fernando/ms/users/app/dfood_users_service/domain/model/User.java
@@ -14,6 +14,7 @@ public class User {
     private Long id;
     private String username;
     private String password;
+    private String fullName;
     private String email;
     private TypeUser typeUser;
     private Boolean verify;
